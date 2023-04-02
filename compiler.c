eb8d573dfe13e17e8234a47ad68e78603d7b8c7f
TheProgxy <theprogxy@gmail.com>
2023-04-02 18:15:12 +0200

    fix

--- compiler.c
+++ compiler.c
@@ -189,7 +189,6 @@ static void assembleProgram(char** data) {
     
     for (int i = 0; i < linesCount; i++) {
         if (!strlen(data[i])) {
-            printf("\nThe string at line: %d is empty!", i + 1);
             continue;
         }
 
